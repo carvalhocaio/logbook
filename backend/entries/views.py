1440cd750e8b1460774dc41ab766344f5587c56d
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-06-10 08:45:05 -0400

    Add Entries app with models, views, templates, and URL routing

--- backend/entries/views.py
+++ backend/entries/views.py
@@ -1,2 +1,11 @@
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+from .models import Entry
+
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+class EntryDetailView(DetailView):
+    model = Entry
