6dae2a92fdbe6e5ccb8899854225be31549e6c32
Caio Carvalho <caiocarvalho.py@gmail.com>
2025-09-08 15:43:51 -0400

    feat: displaying the LogBook Entries on the Front End

--- entries/views.py
+++ entries/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.views.generic import DetailView, ListView
+
+from .models import Entry
+
+
+class EntryListView(ListView):
+    model = Entry
+    queryset = Entry.objects.all().order_by("-date_created")
+
+
+class EntryDetailView(DetailView):
+    model = Entry
